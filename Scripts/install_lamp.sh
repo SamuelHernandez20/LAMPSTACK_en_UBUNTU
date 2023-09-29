b154a5492a7f766ae458887225d48ff8f8c7175e
Ubuntu <ubuntu@ip-172-31-49-127.ec2.internal>
2023-09-29 12:34:52 +0000

    Añadiendo scripts

---
+++ Scripts/install_lamp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Muestra comandos que se van ejecutando
+set -x
+
+echo "Esto es una prueba"
+
+# Actualizamos los repos
+
+#sudo apt update
+
+# Actualizar paquetes 
+
+#sudo apt upgrade
+
+#Instalar apache
+
+sudo apt install apache2 -y
+
+
